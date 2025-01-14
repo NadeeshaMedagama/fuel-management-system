2ea5aa83f0541e47aaf2f875c2b955856e4946f4
Nadeesha Medagama <nadeeshamedagama@gmail.com>
2025-01-15 00:11:56 +0530

    started fuel backend

---
+++ fuel-management-system/src/main/java/com/example/fuel/management/system/FuelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuel.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelManagementSystemApplication.class, args);
+	}
+
+}
